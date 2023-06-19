d2530fd87acaa254b31f11ead9c05a09c3e278e3
Zeynep Uygun <zynpzynp.uygun@gmail.com>
2023-06-19 10:44:23 +0300

    taslak

---
+++ src/main/java/com/example/demo/MyController.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/book")
+public class MyController {
+
+
+    @GetMapping
+    public String viewBooks(Model model) {
+        return "book";
+    }
+}
---
+++ src/main/webapp/WEB-INF/jsp/book.jsp
@@ -0,0 +1,20 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<html>
+<head>
+    <title>View Books</title>
+</head>
+<body>
+<table>
+    <thead>
+    <tr>
+        <th>WELCOME</th>
+        <th>OK</th>
+        <th>BB</th>
+    </tr>
+    </thead>
+
+</table>
+</body>
+</html>
+ÇALIŞIYOR
